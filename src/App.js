65eef6a149d8e5eff699a8d06a9bcb212310348c
Ankit-Srivastav <ankit-srivastav@Ankit-Srivastavs-MacBook-Air.local>
2025-07-28 11:34:45 +0530

    Initial commit - E-COM project with frontend and backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './component/Nav';
+import Footer from './component/Footer';
+import Register from './component/Register';
+import PrivateComponent from './component/PrivateComponent';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route element={<PrivateComponent/>}>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/product" element={<h2>Product Page</h2>} />
+        <Route path="/add" element={<h2>Add Product Page</h2>} />
+        <Route path="/update" element={<h2>Update Product Page</h2>} />
+        <Route path="/logout" element={<h2>Logout Page</h2>} />
+        <Route path="/profile" element={<h2>Profile Page</h2>} />
+      </Route>
+        <Route path="/register" element={ <Register/>
+      } />
+      </Routes>
+      </BrowserRouter>
+       <Footer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/component/Register.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Register = () => {
+    const Navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    let user = localStorage.getItem('user')
+const navigate = useNavigate();
+useEffect(()=>{
+    if (user){
+        navigate('/')
+    }
+})
+    const handleSubmit = async () => {
+        let responce = await fetch('http://localhost:3007/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name, email, password
+            })
+
+        }
+        )
+        if (responce.ok){
+            responce = await responce.json()
+            localStorage.setItem('user', JSON.stringify(responce))
+            console.log(responce, "responce");
+            Navigate('/'); 
+        }
+
+        setEmail('');
+        setPassword('');
+        setName('');
+    }
+    return (
+        <div className="register-page">
+            <h2>Register Page</h2>
+            <form>
+                <input className="input-field" value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder="Name" />
+                <input className="input-field" value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Email" />
+                <input className="input-field" value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder="Password" />
+                <button className="submit-button" type="button" onClick={handleSubmit}>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
