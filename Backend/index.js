0e6bb6a16368fe244d146f327df7237379936cc4
Ankit-Srivastav <ankit-srivastav@Ankit-Srivastavs-MacBook-Air.local>
2025-07-28 11:07:04 +0530

    Added .gitignore and initial project setup

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('./db/config');
+const users = require('./db/User');
+const cors = require('cors');
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.post('/register' ,async (req, resp) => {
+    const userData = users(req.body);
+    const result = await userData.save();
+    resp.send(result)
+})
+
+app.listen(3007);
