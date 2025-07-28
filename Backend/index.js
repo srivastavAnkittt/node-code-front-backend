31d871e33a4a8ce39177e838db41f758449e0790
Ankit-Srivastav <ankit-srivastav@Ankit-Srivastavs-MacBook-Air.local>
2025-07-28 14:40:36 +0530

    Initial commit for frontend

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,33 @@ const express = require('express');
 require('./db/config');
 const users = require('./db/User');
 const cors = require('cors');
+// const { use } = require('react');
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.post('/register' ,async (req, resp) => {
+app.post('/register', async (req, resp) => {
     const userData = users(req.body);
-    const result = await userData.save();
+    let result = await userData.save();
+    result = result.toObject();
+    delete result.password;
     resp.send(result)
+    console.log(result);
 });
 
-app.listen(5000);
+app.post('/login', async (req, resp) => {
+    let {email, password } = req.body;
+if (email  && password){
+    let user = await users.findOne({email, password}).select("-password");
+    if(user){
+        resp.send(user);
+        console.log(user);  
+    }
+}else{
+    resp.send({result: "user not found"})
+    console.log('user not found')
+}
+})
+
+
+app.listen(3007);
